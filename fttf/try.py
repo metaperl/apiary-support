4e6b8b058511a6e5fd0ff5a142b2b164ef995cd2
Terrence Brannon <metaperl@gmail.com>
2015-04-02 17:05:48 -0700

    closer to plotting candlesticks!

--- fttf/try.py
+++ fttf/try.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy
 import oandapy
 import talib
+from talib import MA_Type
 
 oanda = oandapy.API(
     environment="practice",
@@ -20,42 +21,49 @@ print prices
 
 class Trader(object):
 
-    def __init__(self, oanda, granularity):
+    def __init__(self, oanda, granularity, period=50):
         self.oanda = oanda
         self.granularity=granularity
+        self.period = 50
+
+    def get_history(self):
+        self.history = self.oanda.get_history(
+            instrument="EUR_USD",
+            count=self.period,
+            granularity=self.granularity)
 
     def sma(self, period=50):
-        response = self.oanda.get_history(
-            instrument="EUR_USD",
-            count=period,
-            granularity=self.granularity)
-        candles = response.get("candles")
-        candles = numpy.array([candle['closeAsk'] for candle in candles])
+        candles = self.history.get("candles")
         print candles, type(candles)
+        candles = numpy.array([candle['closeAsk'] for candle in candles])
         sma = talib.SMA(candles, timeperiod=period)
         print sma
 
+    @staticmethod
+    def drawbox(a):
+        figure = plt.figure()
+        ax = figure.add_subplot(111)
+        ax.plot(a)
+        plt.show()
+
     def stochastic(self, fast_k=14, slow_k=3, fast_d=3, slow_d=3):
-        candles = self.oanda.get_history(
-            instrument="EUR_USD",
-            granularity=self.granularity)['candles']
+        candles = self.history.get('candles')
         inputs = {
             'low': numpy.array([candle['lowAsk'] for candle in candles]),
             'high': numpy.array([candle['highAsk'] for candle in candles]),
             'close': numpy.array([candle['closeAsk'] for candle in candles])
             }
+        self.drawbox(inputs['high'])
         slowk, slowd = talib.abstract.STOCH(
-            inputs, fast_k, slow_k, 0, slow_d, 0)
+            inputs, fast_k, slow_k, MA_Type.SMA, slow_d, MA_Type.EMA)
         print "SLOWK, SLOWD", slowk, slowd, type(slowk), type(slowd)
-        figure = plt.figure()
-        ax = figure.add_subplot(111)
-        ax.plot(slowk)
-        plt.show()
-        print 'showing'
+        self.drawbox(slowk)
+        self.drawbox(slowd)
 
 
 
 t = Trader(oanda, 'H1')
+t.get_history()
 t.sma()
 t.stochastic()
 
